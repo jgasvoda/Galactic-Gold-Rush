f7da0c23f9e4caea1c04d4502a795cb79aada1c3
Jeffrey Gasvoda <Jeffrey Gasvoda@DESKTOP-VIAGSKQ>
2020-06-28 14:41:04 -0500

    Object name changes, prep for code integration

--- Assets/PieceSelect.cs
+++ Assets/PieceSelect.cs
@@ -7,12 +7,17 @@ public class PieceSelect : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void OnMouseDown()
+    {
+        GetComponent<Renderer>().material.SetColor("_Color", new Color32(255, 171, 0, 255));
     }
 }
